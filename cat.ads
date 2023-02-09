29e9fbb9562211290263e96a9945239f280d3702
chaimalk <108950943+chaimalk@users.noreply.github.com>
2023-02-09 02:03:01 +0100

    Create cat.ads

---
+++ cat.ads
@@ -0,0 +1,14 @@
+{
+  "AdsController": {
+    "adNetwork": "unity",
+    "admobBanner": "ca-app-pub-1010770342437944/1711074800",
+    "admobInterstitial": "ca-app-pub-1010770342437944/2832584800",
+    "applovinBanner": "d4584c0dccceb4db",
+    "applovinInterstitial": "e511223669e070a7",
+    "unityGameId": "5156181",
+    "testMode": true,
+    "unityBanner": "Banner_Android",
+    "unityInterstitial": "Interstitial_Android",
+    "reviewActive": true
+  }
+}
